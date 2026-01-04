c79c2aba1189e3905d2267d174439f90b62b2527
SHerlihy <steven_herlihy@yahoo.com>
2026-01-04 18:24:58 +0000

    diagram source routes

--- planning/infrastructure_diagrams/service_flow/diagram.js
+++ planning/infrastructure_diagrams/service_flow/diagram.js
@@ -1,7 +1,7 @@
 const serviceFlow = `
     flowchart LR
 
-    Client <--> API
+    Client <--> STAGE
     
     subgraph GRP[Get Role Policy]
         subgraph GRPAllow[Allow]
@@ -11,24 +11,12 @@ const serviceFlow = `
 
     subgraph AWSIAM[IAM Policies]
         subgraph LAIM[Lambda IAM]
-            subgraph DRP[Delete Role Policy]
-                subgraph DRPAllow[Allow]
-                    DeleteDoc[lambda > s3:DeleteObject]
-                end
-            end
-
             subgraph URP[Upload Role Policy]
                 subgraph URPAllow[Allow]
                     UploadDoc[lambda > s3:PutObject]
                 end
             end
 
-            subgraph GRP[Get Role Policy]
-                subgraph GRPAllow[Allow]
-                    GetIds[lambda > s3:GetObject]
-                end
-            end
-    
             subgraph LRP[Lambda Resource Policy]
                 subgraph LRPAllow[Allow]
                     APILambda["API > lambda:InvokeFunction"]
@@ -40,47 +28,50 @@ const serviceFlow = `
     subgraph AWS
     subgraph global
 
-        GRP -.- List
-        GRP -.- Get
-        URP -.- Upload
-        DRP -.- Delete
-        
-        Bucket -.-> GetIds
-        Bucket -.-> UploadDoc
-        Bucket -.-> DeleteDoc
-        
-        LRP -.- Auth
-        LRP -.- List
-        LRP -.- Get
-        LRP -.- Upload
-        LRP -.- Delete
-        
-        API -.-> APILambda
+        STAGE[main]
+        KBAAS{kbaas}
 
-        API{API Gateway}
-        Auth[authoriser lambda]
+        PREFLIGHT[preflight]
+
+        QUERY[query]
         
-        API <--> Auth
+        STAGE --> KBAAS
+        
+        KBAAS --> PREFLIGHT
+        PREFLIGHT --> KBAAS
 
-        API <-- GET: / --> List
-        API <-- GET: /?ids=[ids] --> Get
-        API <-- POST: / --> Upload
-        API <-- DELETE: /?ids=[ids] --> Delete
+        KBAAS <--> LIST & PHRASES & QUERY
+        PREFLIGHT ~~~ LIST & PHRASES & QUERY
 
-        subgraph region
-            subgraph authorised
-                List[Get all IDs]
-                Get[Get by IDs]
-                Upload[Upload single]
-                Delete[Delete by IDs]
+        AUTH[authoriser lambda]
+
+        LIST --> AUTH
+        AUTH --> LIST
+
+        PHRASES --> AUTH
+        AUTH --> PHRASES
+        
+        subgraph source
+
+            LIST[list]
+            PHRASES[phrases]
+            
+            LIST <-- GET: / --> BUCKET
+            PHRASES <-- PUT: / --> OBJECT
+
+            subgraph region
+
+                subgraph bucket
+                    BUCKET[S3 Bucket]
+                    
+                    subgraph objects
+                        OBJECT[Phrases Object]
+                    end
+
+                end
+
             end
 
-            Bucket[S3 Bucket]
-
-            List <--> Bucket
-            Get <--> Bucket
-            Upload <--> Bucket
-            Delete <--> Bucket
         end
     end
     end
